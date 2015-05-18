4284c1f5f98ba2412a89041a2d6322f63f9bb3b8
frank.yu <frank.yu@daocloud.io>
2015-05-18 08:31:19 +0800

    code update, remove words

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ def hello_world():
   storage = Storage()
   storage.populate()
   score = storage.score()
-  return "Hello world & Docker Run, yes Docker Run Test  Demo Demo Demo %d!" % score
+  return "Hello world & Docker Run %d!" % score
 
 class Storage():
   def __init__(self):
